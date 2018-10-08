c9eb7b52c457ce7bb213fc2638d8cfa5d2ec1fac
Andrey Radionov <andyomsk@gmail.com>
2018-10-08 21:43:47 +0300

    MainActivity recycler initialization with Adapter and LayoutManager FAB clickListener

--- app/src/main/java/com/tinkoff/androidcourse/MainActivity.java
+++ app/src/main/java/com/tinkoff/androidcourse/MainActivity.java
@@ -3,14 +3,25 @@ package com.tinkoff.androidcourse;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int TEST_WORKERS_COUNT = 5;
+
+    private RecyclerView workersRecyclerView;
+    private WorkersAdapter workersAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        workersRecyclerView = findViewById(R.id.recycler_view);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -19,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
                 /**
                  * Реализовать добавление тестовых работников
                  */
+                Worker worker = WorkerGenerator.generateWorker();
+                workersAdapter.addWorker(worker);
+                workersRecyclerView.smoothScrollToPosition(workersAdapter.getItemCount());
             }
         });
 
@@ -28,5 +42,11 @@ public class MainActivity extends AppCompatActivity {
          *
          * Тестовые данные для отображения генерятся WorkerGenerator
          */
+        List<Worker> testWorkers = WorkerGenerator.generateWorkers(TEST_WORKERS_COUNT);
+        workersAdapter = new WorkersAdapter(testWorkers);
+        workersRecyclerView.setAdapter(workersAdapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        workersRecyclerView.setLayoutManager(layoutManager);
     }
 }
